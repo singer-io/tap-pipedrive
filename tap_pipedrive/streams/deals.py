01f2f8e35203e4483283fe75decc26f02026c9a4
Eduard Kracmar <eduard.kracmar@hotovo.org>
2017-09-06 22:34:56 +0200

    Schema fixes for Currencies and Deals

--- tap_pipedrive/streams/deals.py
+++ tap_pipedrive/streams/deals.py
@@ -1,29 +1,6 @@
 from tap_pipedrive.singer.stream import Stream
 
 
-class CurrenciesStream(Stream):
-    endpoint = 'currencies'
-    schema = {
-        "type": "object",
-        "properties": {
-            "code": {
-                "type": ["null", "string"]
-            },
-            "name": {
-                "type": ["null", "string"]
-            },
-            "decimal_points": {
-                "type": ["null", "integer"]
-            },
-            "symbol": {
-                "type": ["null", "string"]
-            },
-            "active_flag": {
-                "type": ["null", "boolean"]
-            },
-            "is_custom_flag": {
-                "type": ["null", "boolean"]
-            },
-        }
-    }
+class DealsStream(Stream):
+    endpoint = 'deals'
     key_properties = ['id', ]
