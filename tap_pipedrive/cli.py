b4d6c3bf6ac52e121a8463a7dd38abfbaf63d307
Eduard Kracmar <eduard.kracmar@hotovo.org>
2017-09-07 22:15:18 +0200

    Added handling of unsupported --discover and --properties

--- tap_pipedrive/cli.py
+++ tap_pipedrive/cli.py
@@ -4,8 +4,20 @@ import singer
 from tap_pipedrive.tap import PipedriveTap
 
 
+logger = singer.get_logger()
+
+
 def main():
     args = singer.utils.parse_args(['api_token'])
+
+    if args.properties:
+        logger.error('Tap does not support --properties')
+        exit(1)
+
+    if args.discover:
+        logger.error('Tap does not support -d/--discover')
+        exit(1)
+
     pipedrive_tap = PipedriveTap(args.config, args.state)
     pipedrive_tap.do_sync()
 
