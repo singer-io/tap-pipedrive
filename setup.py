bd5a9abd555860c71d8131d7a7a6bc50e13ce5bb
Anne Maiale <anne@stitchdata.com>
2018-06-27 13:16:10 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name="tap-pipedrive",
-      version="0.0.15",
+      version="0.0.16",
       description="Singer.io tap for extracting data from the Pipedrive API",
       author="Stitch",
       author_email="dev@stitchdata.com",
