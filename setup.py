c7c0b9a301a220aec5ccb5904953f20a6bbb6a47
nick-mccoy <nick@stitchdata.com>
2019-05-08 12:50:42 +0000

    version to 0.2.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name="tap-pipedrive",
-      version="1.0.1",
+      version="0.2.5",
       description="Singer.io tap for extracting data from the Pipedrive API",
       author="Stitch",
       author_email="dev@stitchdata.com",
