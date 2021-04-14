834a08030ded3134b074b43b3f3799de4f328b19
dbshah1212 <dhruvin.shah@crestdatasys.com>
2021-04-14 17:37:02 +0530

    TDL-731: Added discovery test from tap-tester to test folder.

---
+++ tests/test_pipedrive_discovery.py
@@ -0,0 +1,134 @@
+import tap_tester.connections as connections
+import tap_tester.menagerie   as menagerie
+import tap_tester.runner      as runner
+import unittest
+import os
+
+class PipedriveDiscovery(unittest.TestCase):
+    def environment_variables(self):
+        return {"TAP_PIPEDRIVE_API_TOKEN"}
+
+    def setUp(self):
+        missing_envs = [x for x in self.environment_variables() if os.getenv(x) is None]
+        if missing_envs:
+            raise Exception("Missing test-required environment variables: {}".format(missing_envs))
+
+    def expected_check_streams(self):
+        return {
+            'files',
+            'activities',
+            'dealflow',
+            'deal_products',
+            'activity_types',
+            'persons',
+            'currency',
+            'pipelines',
+            'notes',
+            'stages',
+            'products',
+            'organizations',
+            'users',
+            'delete_log',
+            'filters',
+            'deals'
+        }
+
+    def expected_pks(self):
+        return {'activities': {'id'},
+                'activity_types': {'id'},
+                'currency': {'id'},
+                'deal_products': {'id'},
+                'dealflow': {'id'},
+                'deals': {'id'},
+                'delete_log': {'id'},
+                'files': {'id'},
+                'filters': {'id'},
+                'notes': {'id'},
+                'organizations': {'id'},
+                'persons': {'id'},
+                'pipelines': {'id'},
+                'products': {'id'},
+                'stages': {'id'},
+                'users': {'id'}}
+
+    def name(self):
+        return "tap_tester_pipedrive_discovery"
+
+    def tap_name(self):
+        return "tap-pipedrive"
+
+    def get_type(self):
+        return "platform.pipedrive"
+
+    def get_credentials(self):
+        return {'api_token': os.getenv('TAP_PIPEDRIVE_API_TOKEN')}
+
+    def get_properties(self):
+        return {'start_date' : "2019-09-21T00:00:00Z"}
+
+    def test_run(self):
+        conn_id = connections.ensure_connection(self)
+
+        # run in check mode
+        check_job_name = runner.run_check_mode(self, conn_id)
+
+        # verify check  exit codes
+        exit_status = menagerie.get_exit_status(conn_id, check_job_name)
+        menagerie.verify_check_exit_status(self, exit_status, check_job_name)
+
+        # verify the tap discovered the right streams
+        catalog = menagerie.get_catalogs(conn_id)
+
+        # assert we find the correct streams
+        self.assertEqual(self.expected_check_streams(),
+                         {c['tap_stream_id'] for c in catalog})
+
+        known_replication_keys = {
+            'persons': ['update_time'],
+            'stages': ['update_time'],
+            'files': ['update_time'],
+            'activity_types': ['update_time'],
+            'deal_products': [],
+            'pipelines': ['update_time'],
+            'dealflow': ['log_time'],
+            'users': [],
+            'activities': ['update_time'],
+            'delete_log': [],
+            'currency': [],
+            'products': ['update_time'],
+            'filters': ['update_time'],
+            'notes': ['update_time'],
+            'organizations': ['update_time'],
+            'deals': ['update_time']}
+        for tap_stream_id in self.expected_check_streams():
+            found_stream = [c for c in catalog if c['tap_stream_id'] == tap_stream_id][0]
+            found_key_properties = set(found_stream['key_properties'])
+            stream_metadata = found_stream['metadata']
+
+            # assert that the pks are correct
+            self.assertEqual(self.expected_pks()[tap_stream_id],
+                             found_key_properties)
+
+            # Check that key properties have inclusion automatic
+            for prop in found_key_properties:
+                metadata_entries = [m['metadata'] for m in stream_metadata if m['breadcrumb'] == ['properties', prop]]
+                self.assertEqual(len(metadata_entries), 1,
+                                 msg="Found more or less than one metadata entry for key property {} in stream {}".format(
+                                     prop, tap_stream_id))
+                self.assertEqual(metadata_entries[0]['inclusion'], 'automatic',
+                                 msg="Inclusion for key property {} in stream {} is not automatic".format(
+                                     prop, tap_stream_id))
+
+            # Check that all else have inclusion available
+            non_key_property_metadata = [m for m in stream_metadata if m['breadcrumb'][1] not in (self.expected_pks()[tap_stream_id].union(known_replication_keys[tap_stream_id]))]
+            for metadata_entry in non_key_property_metadata:
+                inclusion = metadata_entry['metadata']['inclusion']
+                self.assertEqual(inclusion, 'available',
+                                 msg="Inclusion for property {} in stream {} is not available, was: {}".format(
+                                     metadata_entry['breadcrumb'][1],
+                                     tap_stream_id,
+                                     inclusion))
+
+            # Verify after discovery that selected is None for all fields
+            for metadata_entry in stream_metadata:
+                self.assertNotIn('selected', metadata_entry['metadata'].keys())
