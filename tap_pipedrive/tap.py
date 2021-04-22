cc5cbf3c8ce45798d86c8e526b91901effa18fdf
Savan Chovatiya <savan.chovatiya@CDSYS.LOCAL>
2021-04-22 10:12:04 +0530

    TDL-12667:Changed info message for waiting time as per suggestion

--- tap_pipedrive/tap.py
+++ tap_pipedrive/tap.py
@@ -89,7 +89,7 @@ def retry_after_wait_gen():
         exc_info = sys.exc_info()
         resp = exc_info[1].response
         sleep_time_str = resp.headers.get('X-RateLimit-Reset')
-        logger.info("Received 429 -- sleeping for %s seconds", sleep_time_str)
+        logger.info("API rate limit exceeded -- sleeping for %s seconds", sleep_time_str)
         yield math.floor(float(sleep_time_str))
 
 
