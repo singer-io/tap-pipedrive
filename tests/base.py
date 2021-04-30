8a06e789126a5da4cd537014231e1dc0a190f598
kspeer825 <kspeer@talend.com>
2021-04-30 16:29:49 +0000

    drop foreign keys from  expected  auto fields:

--- tests/base.py
+++ tests/base.py
@@ -185,8 +185,7 @@ class PipedriveBaseTest(unittest.TestCase):
     def expected_automatic_fields(self):
         auto_fields = {}
         for k, v in self.expected_metadata().items():
-            auto_fields[k] = v.get(self.PRIMARY_KEYS, set()) | v.get(self.REPLICATION_KEYS, set()) \
-                | v.get(self.FOREIGN_KEYS, set())
+            auto_fields[k] = v.get(self.PRIMARY_KEYS, set()) | v.get(self.REPLICATION_KEYS, set())
         return auto_fields
 
     def expected_replication_method(self):
