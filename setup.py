1fbdffdf983a2e5326015a462be0b812636b33da
Collin Simon <csimon@talend.com>
2021-05-26 13:25:14 +0000

    Bump to v1.1.0 and update changelog

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name="tap-pipedrive",
-      version="1.0.6",
+      version="1.1.0",
       description="Singer.io tap for extracting data from the Pipedrive API",
       author="Stitch",
       author_email="dev@stitchdata.com",
