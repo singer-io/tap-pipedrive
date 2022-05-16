315abd517bb503bdc11dee204910a72b48cce176
savan-chovatiya <savan.chovatiya@crestdatasys.com>
2022-05-16 12:29:01 +0530

    Updated all_field test to exclude fields

--- tests/test_pipedrive_all_fields.py
+++ tests/test_pipedrive_all_fields.py
@@ -6,6 +6,29 @@ class PipedriveAllFields(PipedriveBaseTest):
 
     """Ensure running the tap with all streams and fields selected results in the replication of all fields."""
 
+    # Custom fields, not able to generate data for these fields.
+    fields_to_remove = {
+        "organizations": {
+            "timeline_last_activity_time",
+            "timeline_last_activity_time_by_owner",
+            "reference_activities_count",
+        },
+        "persons": {
+            "timeline_last_activity_time",
+            "timeline_last_activity_time_by_owner",
+            "reference_activities_count",
+        },
+        "files": {"email_message_id", "note_id"},
+        "users": {"activated"},
+        "deals": {
+            "product_amount",
+            "pipeline",
+            "reference_activities_count",
+            "product_quantity",
+        },
+        "products": {"unit_prices", "price"},
+    }
+
     def name(self):
         return "tap_tester_pipedrive_all_fields"
 
@@ -14,7 +37,7 @@ class PipedriveAllFields(PipedriveBaseTest):
         """
         Assert given conditions:
         • verify no unexpected streams are replicated
-        • Verify that more than just the automatic fields are replicated for each stream. 
+        • Verify that more than just the automatic fields are replicated for each stream.
         • Verify all fields for each streams are replicated
         """
 
@@ -53,7 +76,7 @@ class PipedriveAllFields(PipedriveBaseTest):
 
         # verify no unexpected streams are replicated
         synced_streams_names = set(stream_to_record_count.keys())
-        self.assertSetEqual(expected_streams,synced_streams_names)
+        self.assertSetEqual(expected_streams, synced_streams_names)
 
         for stream in expected_streams:
             with self.subTest(stream=stream):
@@ -75,6 +98,11 @@ class PipedriveAllFields(PipedriveBaseTest):
                         )
 
                 # verify all fields for each stream are replicated
+                # remove some fields as data cannot be generated / retrieved
+                fields = self.fields_to_remove.get(stream) or []
+                for field in fields:
+                    expected_all_keys.remove(field)
+
                 self.assertSetEqual(
                     expected_all_keys,
                     actual_all_keys,
