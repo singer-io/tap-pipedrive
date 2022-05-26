e815a6ad15757b80c3d1694a45cec21df0638066
savan-chovatiya <80703490+savan-chovatiya@users.noreply.github.com>
2022-05-26 11:30:43 +0530

    TDL-18852: Add missing tap tester tests (#113)
    * added missing tap-tester tests
    
    * Added missing test cases for discovery test
    
    * Updated all_field test to exclude fields
    
    * Remove extra print statements
    
    * Resolved bookmark test error
    
    Co-authored-by: namrata270998 <namrata.brahmbhatt@crestdatasys.com>
    Co-authored-by: harshpatel4crest <harsh.patel4@crestdatasys.com>

---
+++ tests/test_pipedrive_automatic_fields.py
@@ -0,0 +1,55 @@
+from tap_tester import runner, connections
+from base import PipedriveBaseTest
+
+class PipedriveAutomaticFieldsTest(PipedriveBaseTest):
+
+    def name(self):
+        return "tap_tester_pipedrive_automatic_fields_test"
+
+    def test_run(self):
+        """
+        Testing that all the automatic fields are replicated despite de-selecting them
+        - Verify that only the automatic fields are sent to the target.
+        - Verify that all replicated records have unique primary key values.
+        """
+        conn_id = connections.ensure_connection(self)
+        expected_streams = self.expected_streams()
+
+        # run check mode
+        found_catalogs = self.run_and_verify_check_mode(conn_id)
+
+        # table and field selection
+        test_catalogs = [catalog for catalog in found_catalogs
+                                      if catalog.get('stream_name') in expected_streams]
+
+        # Select all streams and no fields within streams
+        self.perform_and_verify_table_and_field_selection(
+            conn_id, test_catalogs, select_all_fields=False)
+
+        # run sync
+        record_count_by_stream = self.run_and_verify_sync(conn_id)
+        synced_records = runner.get_records_from_target_output()
+
+        for stream in expected_streams:
+            with self.subTest(stream=stream):
+
+                # expected values
+                expected_primary_keys = self.expected_primary_keys()[stream]
+                expected_keys = expected_primary_keys | self.expected_replication_keys()[stream]
+
+                # collect actual values
+                messages = synced_records.get(stream)
+                record_messages_keys = [set(row['data'].keys()) for row in messages['messages']]
+
+                # check if the stream has collected some records
+                self.assertGreater(record_count_by_stream.get(stream, 0), 0)
+
+                # Verify that only the automatic fields are sent to the target
+                for actual_keys in record_messages_keys:
+                    self.assertSetEqual(expected_keys, actual_keys)
+
+                # Verify we did not duplicate any records across pages
+                records_pks_list = [tuple([message.get('data').get(primary_key) for primary_key in expected_primary_keys])
+                                           for message in messages.get('messages')]
+                self.assertCountEqual(records_pks_list, set(records_pks_list),
+                                      msg="We have duplicate records for {}".format(stream))
