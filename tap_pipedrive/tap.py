eafb57f354ea65ba3341ebc73a3ba433bb7bff0c
Harsh <80324346+harshpatel4crest@users.noreply.github.com>
2022-06-01 19:12:29 +0530

    TDL-19236: Fix bookmark strategy for dealflow stream (#117)
    * updated bookmark strategy for dealflow stream
    
    * updated the code to keep consistency in bookmark format
    
    * resolved review comments
    
    * removed comment line
    
    * updated test case comments
    
    * updated the test case to check bookmarking for dealflow stream

--- tap_pipedrive/tap.py
+++ tap_pipedrive/tap.py
@@ -222,8 +222,10 @@ class PipedriveTap(object):
                     else:
                         stream.more_items_in_collection = False
 
-                # set the attribution window so that the bookmark will reflect the new initial_state for the next sync
-                stream.earliest_state = stream.stream_start.subtract(hours=3)
+                # Set the bookmark with a minimum from the `now - attribution_window` and maximum replication key
+                # The "stream start date" is in the form of "%Y-%m-%dT%H:%M:%S.%f+00:00", whereas the "earliest_state" is in the
+                # format of "%Y-%m-%dT%H:%M:%S+00:00", thus replacing the "microsecond" part to keep consistency in bookmark format
+                stream.earliest_state = min(stream.earliest_state, stream.stream_start.subtract(hours=3)).replace(microsecond=0)
             else:
                 # paginate
                 self.do_paginate(stream, stream_metadata)
