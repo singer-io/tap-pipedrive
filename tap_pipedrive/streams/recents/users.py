ce652ea4d4ab8a07f79214009fbe8c6ff12e6b04
Dan Mosora <30501696+dmosorast@users.noreply.github.com>
2022-08-09 17:16:06 -0400

    Handle new structure for `users` rows (#119)
    * Handle new structure, with fallback to old structure in case the change is reverted
    
    * Version 1.1.3 and changelog

--- tap_pipedrive/streams/recents/users.py
+++ tap_pipedrive/streams/recents/users.py
@@ -10,4 +10,10 @@ class RecentUsersStream(RecentsStream):
     replication_method = 'FULL_TABLE'
 
     def process_row(self, row):
-        return row['data'][0]
+        # NB> Temporary split here in response to unintended change made with this changelog
+        # - https://developers.pipedrive.com/changelog/post/improvements-to-permissionsets-api-and-users-api
+        # - This used to be a list, but turned into an object with this changelog entry's effective date.
+        if isinstance(row['data'], dict):
+            return row['data']
+        else:
+            return row['data'][0]
