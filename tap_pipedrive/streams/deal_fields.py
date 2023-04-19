7482f4394956f8046b1f59dae271ecc3dbc71f50
kethan1122 <kcherukuri@talend.com>
2023-04-19 10:42:23 +0530

    add deal_fields stream

---
+++ tap_pipedrive/streams/deal_fields.py
@@ -0,0 +1,17 @@
+from tap_pipedrive.stream import PipedriveStream
+
+
+class DealFields(PipedriveStream):
+    endpoint = 'dealFields'
+    schema = 'deal_fields'
+    state_field = 'update_time'
+    key_properties = ['id']
+    replication_method = 'INCREMENTAL'
+
+    def get_name(self):
+        return self.schema
+
+    def process_row(self, row):
+        # Ignore child fields since don't have id(PK) and update_time(replication_key)
+        return None if 'parent_id' in row else row
+
