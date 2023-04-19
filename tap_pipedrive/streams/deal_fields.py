24352ebc3502f60019e7cc20b379b74eab9479b7
kethan1122 <kcherukuri@talend.com>
2023-04-19 17:12:59 +0530

    handle none values for replication_key

--- tap_pipedrive/streams/deal_fields.py
+++ tap_pipedrive/streams/deal_fields.py
@@ -13,13 +13,4 @@ class DealFields(PipedriveStream):
 
     def process_row(self, row):
         # Ignore child fields as they don't have id(PK) and update_time(replication_key)
-        if 'parent_id' in row:
-            return None
-
-        # It is expected that the fields which haven't been updated would have
-        # replication_key value as None
-        # So use add_time as replication_key value
-        if row[self.state_field] is None:
-            row[self.state_field] = row["add_time"]
-
-        return row
+        return None if 'parent_id' in row else row
