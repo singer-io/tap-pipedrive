6a36bafd515325a596d1d5051e10abfc6dd27a75
Andy Lu <andy@stitchdata.com>
2025-07-01 15:32:20 +0000

    Bump versions

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name="tap-pipedrive",
       py_modules=["tap_pipedrive"],
       install_requires=[
           "pendulum==2.1.2",
-          "requests==2.32.3",
+          "requests==2.32.4",
           "singer-python==5.12.1",
       ],
       entry_points="""
