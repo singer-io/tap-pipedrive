881aba1915a255a1ab30003420ec9baa0dd57826
Bacotell36 <ryan@fixdapp.com>
2026-04-03 12:29:56 -0400

    add extra fields to deals

--- tap_pipedrive/streams/deals.py
+++ tap_pipedrive/streams/deals.py
@@ -9,11 +9,22 @@ class DealsStream(DynamicSchemaStream):
     state_field = 'update_time'
     cursor = None
 
+    INCLUDE_FIELDS = (
+        'last_activity_id',
+        'last_incoming_mail_time',
+        'last_outgoing_mail_time',
+        'email_messages_count',
+        'activities_count',
+        'done_activities_count',
+        'undone_activities_count',
+        'participants_count',
+        'files_count',
+        'notes_count',
+        'followers_count',
+    )
+
     def update_request_params(self, params):
-        """
-        Update the request parameters for the stream
-        """
         params = super().update_request_params(params)
         params['status'] = 'open,won,lost,deleted'
-
+        params['include_fields'] = ','.join(self.INCLUDE_FIELDS)
         return params
