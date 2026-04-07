6194717724678303176df5a53472f8e54524b418
Tacobell3669 <73504045+Tacobell3669@users.noreply.github.com>
2026-04-07 11:59:02 -0400

    remove all include_fields other than mail related

--- tap_pipedrive/streams/persons.py
+++ tap_pipedrive/streams/persons.py
@@ -10,17 +10,8 @@ class PersonsStream(DynamicSchemaStream):
     cursor = None
 
     INCLUDE_FIELDS = (
-        'last_activity_id',
         'last_incoming_mail_time',
         'last_outgoing_mail_time',
-        'email_messages_count',
-        'activities_count',
-        'done_activities_count',
-        'undone_activities_count',
-        'participants_count',
-        'files_count',
-        'notes_count',
-        'followers_count',
     )
 
     def update_request_params(self, params):
