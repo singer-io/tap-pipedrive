198cadc72b279d6ee62dedf32e5d80dd0d5f6aa2
Tacobell3669 <73504045+Tacobell3669@users.noreply.github.com>
2026-04-07 11:33:08 -0400

    Update persons.py to include extra fields

--- tap_pipedrive/streams/persons.py
+++ tap_pipedrive/streams/persons.py
@@ -8,3 +8,22 @@ class PersonsStream(DynamicSchemaStream):
     replication_method = 'INCREMENTAL'
     state_field = 'update_time'
     cursor = None
+
+    INCLUDE_FIELDS = (
+        'last_activity_id',
+        'last_incoming_mail_time',
+        'last_outgoing_mail_time',
+        'email_messages_count',
+        'activities_count',
+        'done_activities_count',
+        'undone_activities_count',
+        'participants_count',
+        'files_count',
+        'notes_count',
+        'followers_count',
+    )
+
+    def update_request_params(self, params):
+        params = super().update_request_params(params)
+        params['include_fields'] = ','.join(self.INCLUDE_FIELDS)
+        return params
